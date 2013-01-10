34d0a71dc7d54c441e0839f80f0132cb5a1fa994
Ian Carrico <Ian@FourKitchens.com>
2013-01-09 20:41:35 -0800

    Aurora release 1.1.1

--- template.php
+++ template.php
@@ -12,13 +12,30 @@ function corona_preprocess_maintenance_page(&$vars, $hook) {
   // When a variable is manipulated or added in preprocess_html or
   // preprocess_page, that same work is probably needed for the maintenance page
   // as well, so we can just re-use those functions to do that work here.
-  corona_preprocess_html($variables, $hook);
+  // corona_preprocess_html($variables, $hook);
   // corona_preprocess_page($variables, $hook);
 
   // This preprocessor will also be used if the db is inactive. To ensure your
   // theme is used, add the following line to your settings.php file:
   // $conf['maintenance_theme'] = 'corona';
   // Also, check $vars['db_is_active'] before doing any db queries.
+}
+
+/**
+ * Implements hook_modernizr_load_alter().
+ *
+ * @return
+ *   An array to be output as yepnope testObjects.
+ */
+function corona_modernizr_load_alter(&$load) {
+
+  // We will check for touch events, and if we do load the hammer.js script.
+  $load[] = array(
+    'test' => 'Modernizr.touch',
+    'yep'  => array('/'. drupal_get_path('theme','corona') . '/javascripts/hammer.js'),
+  );
+
+  return $load;
 }
 
 /**
@@ -29,9 +46,9 @@ function corona_preprocess_maintenance_page(&$vars, $hook) {
  * @param $hook
  *   The name of the template being rendered ("html" in this case.)
  */
+/* -- Delete this line if you want to use this function
 function corona_preprocess_html(&$vars) {
-  // yep/nope for conditional JS loading!
-  drupal_add_js(drupal_get_path('theme', 'corona') . '/javascripts/loader.js');
+
 }
 
 /**
